bdc422c5ab029a819cfe09930c422dda8e08556e
tushar <cs20b1098@iiitdm.ac.in>
2023-05-11 00:04:04 +0530

    fixed bugs

--- build.sh
+++ build.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Install node.js
-curl -sL https://deb.nodesource.com/setup_16.x | bash -
-apt-get install -y nodejs
+# brew install node
 
-# Install npm and tailwindcss
-npm install
+# Install tailwindcss globally
 npm install -g tailwindcss
 
 # Compile Tailwind CSS
-npx tailwindcss -i ./static/src/input.css -o ./static/dist/css/output.css --watch
+npx tailwindcss -i ./static/src/input.css -o ./static/dist/css/output.css 
 
-gunicorn app:app
+# gunicorn app:app
